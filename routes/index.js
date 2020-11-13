5e4ca965ff695ec521974388376a0bbf9b766c4d
ThammakornK <chan_tham@hotmail.com>
2020-11-13 20:23:15 +0700

    loginstep1

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,43 @@ var router = express.Router();
 const UserService = require('../services/user')
 const passport = require('passport')
 const jwt = require('jsonwebtoken');
-const { TOKEN_KEYWORD } = require('../util/constant');
+const { TOKEN_KEYWORD, LOGIN_KEY } = require('../util/constant');
 
 
 router.post('/login', async (req, res, next) => {
   const { username, password } = req.body;
   const query = { username }
   try {
+ 
     const userModel = await UserService.find(query)
     if (userModel.length === 0) {
       throw 'Not found user';
     }
-    // TODO: frontend ส่ง password มาดีไหม ควรส่ง hash กับ salt มารึป่าว
-    // TODO: checkout ว่า password ถูกต้องไหม
+   //  TODO: frontend ส่ง password มาดีไหม ควรส่ง hash กับ salt มารึป่าว
+   //  TODO: checkout ว่า password ถูกต้องไหม
     const { _id, role } = userModel[0]
     const token = jwt.sign({ _id, role }, TOKEN_KEYWORD)
-    // TODO: token ต้องมีเวลากำหนดไหม ว่าหมดอายุเมื่อไร
+   //  TODO: token ต้องมีเวลากำหนดไหม ว่าหมดอายุเมื่อไร
     res.send({ token });
   } catch (error) {
     res.status(400).send(error)
   }
 });
 
+router.post('/login_uname', async (req, res, next) => {
+  const { username } = req.body;
+  const query = { username }
+  try {
+ 
+    const userModel = await UserService.find(query)
+    if (userModel.length === 0) {
+      throw 'Not found user';
+    }
+    const {password, salt} = userModel[0]
+    const token = jwt.sign({ password, salt}, LOGIN_KEY)
+    res.send({token});
+  } catch (error) {
+    res.status(400).send(error)
+  }
+});
 module.exports = router;
--- util/constant.js
+++ util/constant.js
@@ -1,3 +1,4 @@
 const TOKEN_KEYWORD = 'lbnFPcIkLeknmYyq'
+const LOGIN_KEY = 'iernhrenherklhnkle'
 
-module.exports = { TOKEN_KEYWORD }
+module.exports = { TOKEN_KEYWORD, LOGIN_KEY }
